9ffa4d9d630c252d8bd711c6aa19cd8d72ab41fd
yumi-jpn <on91.matsumoto.yumi@gmail.com>
2023-08-02 10:35:04 +0900

    add:モーダルを仮追加

--- frontend/app/src/page/RecipePost.js
+++ frontend/app/src/page/RecipePost.js
@@ -1,19 +1,41 @@
 // 管理者のみアクセスできるページにする予定。
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+import Modal from "react-modal";
 
-class RecipePost extends React.Component{
-    render(){
-        return (
-            <div>
-                <h2>レシピ投稿ページ</h2><br/>
-                <Link to='/two' className='NoClassName'>唐揚げ</Link> 
+const RecipePost = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    return (
+        <div>
+            <h2 className="PageTitle">Recipe Post -レシピ新規投稿-</h2><br/>
+
+            <br/>
+            <div className='Wrap'>
+                {/* 新規登録モーダル */}
+                <button onClick={() => setModalOpen(true)}>レシピ新規投稿</button>
+                <Modal isOpen={modalOpen}>
+                    <button onClick={() => setModalOpen(false)}>Close Modal</button>
+                </Modal>
+                {/* レシピ詳細 全てモーダルで内容表示する */}
+                <div className="EachRecipeArea">
+                    <div className="EachRecipe">
+                        <button className="RecipeEdit">編集</button>
+                        <button className="RecipeDelete">削除</button>
+                        <p className="RecipeName">レシピ名</p>
+                    </div>
+                    <div className="EachRecipe">
+                        <button className="RecipeEdit">編集</button>
+                        <button className="RecipeDelete">削除</button>
+                        <p className="RecipeName">レシピ名</p>
+                    </div>
+                </div>
                 <br/>
+
                 <Link to='/'>トップページへ</Link>
-                <br/>
             </div>
-        )
-    } 
+        </div>
+    )
 }
 
 export default RecipePost;
